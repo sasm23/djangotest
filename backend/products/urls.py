1a49ce61fb89f281fb3bfd55985af9fab01725c8
sasm23 <saswatmohanty@outlook.com>
2023-05-06 01:39:13 +0530

    Added the reverse,URLs in serializer

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -6,12 +6,12 @@ urlpatterns = [
     path('',views.product_create_view),
     #path('',views.product_mixin_view),
     #<use mixin view>
-    path('get/',views.product_list_view),
+    path('get/',views.product_list_view, name='products-list'),
     #path('get/',views.product_mixin_view),
     #path('<int:pk>/update/',views.product_update_view),
     path('<int:pk>/update/',views.product_mixin_view),
     #path('<int:pk>/delete/',views.product_delete_view),
     path('<int:pk>/delete/',views.product_mixin_view),
-    path('<int:pk>/',views.product_detail_view)
+    path('<int:pk>/',views.product_detail_view, name='detail_view')
     #path('<int:pk>/',views.product_mixin_view)
 ]
